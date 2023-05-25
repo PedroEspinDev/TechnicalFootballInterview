5edd0d8426e29a18147a3fb45f68d578fd3fdb13
Pedro Espín <muertenegrasanpedro@hotmail.com>
2023-05-25 10:24:55 +0200

    Firt commit is a lie

---
+++ testFootball/src/test/java/com/example/testFootball/TestFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testFootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
