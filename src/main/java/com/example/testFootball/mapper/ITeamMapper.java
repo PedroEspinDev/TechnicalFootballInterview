619c08f18140b347609a6f901e1cc29805fe3078
Pedro Espin <muertenegrasanpedro@hotmail.com>
2023-07-05 09:47:26 +0200

    fix mapper

---
+++ src/main/java/com/example/testFootball/mapper/ITeamMapper.java
@@ -0,0 +1,21 @@
+package com.example.testFootball.mapper;
+
+import com.example.testFootball.dto.TeamDTO;
+import com.example.testFootball.entity.Team;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ITeamMapper {
+    TeamDTO entityToDto(Team entity);
+    Team dtoToEntity(TeamDTO dto);
+    List<Team> dtoToEntityList(List<TeamDTO> dto);
+
+    List<TeamDTO> entityToDtoList(List<Team> entity);
+
+    ITeamMapper INSTANCE = Mappers.getMapper(ITeamMapper.class);
+
+}
